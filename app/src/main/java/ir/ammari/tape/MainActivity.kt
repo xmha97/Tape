72f781cab84b8c7b2bf478939e153cf9bb568f43
Muhammadhussein Ammari <xmha97@gmail.com>
2026-07-09 20:53:07 +0330

    Fix crash on Android 7

--- app/src/main/java/ir/ammari/tape/MainActivity.kt
+++ app/src/main/java/ir/ammari/tape/MainActivity.kt
@@ -6,30 +6,20 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
-import androidx.navigation3.runtime.NavEntry
-import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.ui.NavDisplay
-
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
 
 import ir.ammari.tape.ui.theme.TapeTheme
 
@@ -73,32 +63,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
 
-    val backStack = remember {
-        mutableStateListOf<NavKey>(Home)
-    }
+    val backStack = rememberNavBackStack(Home)
 
     NavDisplay(
         backStack = backStack,
         onBack = {
             if (backStack.size > 1) {
-                backStack.removeLast()
+                backStack.removeLastOrNull()
             }
         },
-        entryProvider = { key ->
-            when (key) {
-                Home -> NavEntry(key) {
-                    HomeScreen(
-                        onOpenSettings = {
-                            backStack.add(Settings)
-                        }
-                    )
-                }
-
-                Settings -> NavEntry(key) {
-                    SettingsScreen()
-                }
-
-                else -> error("Unknown key: $key")
+        entryProvider = entryProvider {
+            entry<Home> {
+                HomeScreen(
+                    onOpenSettings = {
+                        backStack += Settings
+                    }
+                )
+            }
+            entry<Settings> {
+                SettingsScreen()
             }
         },
         modifier = modifier
